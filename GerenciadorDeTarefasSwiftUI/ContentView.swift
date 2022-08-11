be5d95e3997e1ba0baea4cd962c8011aa049ccb5
jpfeijo <j.feijo@edu.pucrs.br>
2022-08-10 23:31:11 -0300

    Initial Commit

---
+++ GerenciadorDeTarefasSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GerenciadorDeTarefasSwiftUI
+//
+//  Created by João Pedro Feijó on 8/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GerenciadorDeTarefasSwiftUI/GerenciadorDeTarefasSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GerenciadorDeTarefasSwiftUIApp.swift
+//  GerenciadorDeTarefasSwiftUI
+//
+//  Created by João Pedro Feijó on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GerenciadorDeTarefasSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
